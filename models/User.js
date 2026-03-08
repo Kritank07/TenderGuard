ca66226397c73d370b0c253ac7d5983fc8aed5d2
Kritank <jainkritank0703@gmail.com>
2026-03-08 08:08:08 +0530

    Revert Auth0 integration

--- models/User.js
+++ models/User.js
@@ -27,6 +27,7 @@ const UserSchema = new mongoose.Schema({
     },
     password: {
         type: String,
+        required: [true, 'Please add a password'],
         minlength: 6,
         select: false
     },
